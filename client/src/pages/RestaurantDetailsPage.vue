9f4fa36fc4175e3a34f3be83bd61f07ec796d468
Jeremy Fowler <jembyfowler@gmail.com>
2024-04-03 14:05:45 -0600

    get by id works

--- client/src/pages/RestaurantDetailsPage.vue
+++ client/src/pages/RestaurantDetailsPage.vue
@@ -4,8 +4,28 @@
 
 
 <script>
+import Pop from '../utils/Pop.js'
+import { restaurantsService } from '../services/RestaurantsService.js'
+import { watch } from 'vue';
+import { useRoute } from 'vue-router';
 export default {
   setup() {
+    const route = useRoute()
+
+
+    async function getRestaurantById(restaurantId) {
+      try {
+        await restaurantsService.getRestaurantById(restaurantId)
+      }
+      catch (error) {
+        Pop.error(error);
+      }
+    }
+
+    watch(() => route.params.restaurantId, () => {
+      getRestaurantById(route.params.restaurantId)
+    }, { immediate: true })
+
     return {}
   }
 }
--- client/src/services/RestaurantsService.js
+++ client/src/services/RestaurantsService.js
@@ -4,6 +4,10 @@ import { logger } from "../utils/Logger.js"
 import { api } from "./AxiosService.js"
 
 class RestaurantsService {
+  async getRestaurantById(restaurantId) {
+    const res = await api.get(`api/restaurants/${restaurantId}`)
+    logger.log('GOT RESTAURANT', res.data)
+  }
   async getRestaurants() {
     const res = await api.get('api/restaurants')
     logger.log('GETTING RESTAURANTS', res.data)
