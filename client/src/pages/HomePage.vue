4ed023d48c9f818f9a971dd10b96b81cd8d78094
Jeremy Fowler <jembyfowler@gmail.com>
2024-04-03 12:35:48 -0600

    drawing restaurant card

--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -1,11 +1,38 @@
 <template>
-  <div>Chill out</div>
+  <div class="container">
+    <section class="row">
+      <div v-for="restaurant in restaurants" :key="restaurant.id" class="col-md-4">
+        <div>
+          <img :src="restaurant.imgUrl" :alt="restaurant.name" class="img-fluid">
+          <div class="p-2">
+            <p class="text-success">
+              <b>{{ restaurant.name }}</b>
+            </p>
+            <p>{{ restaurant.description }}</p>
+            <div class="d-flex justify-content-between align-items-center">
+              <p>
+                <i class="mdi mdi-account-multiple me-1 text-success fs-3"></i>
+                <b>{{ restaurant.visits }}</b>
+                <span> recent visits</span>
+              </p>
+              <p>
+                <i class="mdi mdi-file-document-multiple me-1 text-success fs-3"></i>
+                <b>0</b>
+                <span> reports</span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
-import { onMounted } from 'vue';
+import { computed, onMounted } from 'vue';
 import { restaurantsService } from '../services/RestaurantsService.js';
 import Pop from '../utils/Pop.js';
+import { AppState } from '../AppState.js';
 
 export default {
   setup() {
@@ -21,7 +48,7 @@ export default {
     onMounted(getRestaurants)
 
     return {
-
+      restaurants: computed(() => AppState.restaurants)
     }
   }
 }
--- client/src/services/RestaurantsService.js
+++ client/src/services/RestaurantsService.js
@@ -1,3 +1,5 @@
+import { AppState } from "../AppState.js"
+import { Restaurant } from "../models/Restaurant.js"
 import { logger } from "../utils/Logger.js"
 import { api } from "./AxiosService.js"
 
@@ -5,6 +7,7 @@ class RestaurantsService {
   async getRestaurants() {
     const res = await api.get('api/restaurants')
     logger.log('GETTING RESTAURANTS', res.data)
+    AppState.restaurants = res.data.map(pojo => new Restaurant(pojo))
   }
 }
 
