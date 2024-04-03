2aec7b8d19635e8b096f5b32e9ad33e5a6575c1d
Jeremy Fowler <jembyfowler@gmail.com>
2024-04-03 09:43:19 -0600

    set up table

--- server/dbSetup.sql
+++ server/dbSetup.sql
@@ -1,8 +1,26 @@
-CREATE TABLE IF NOT EXISTS accounts(
-  id VARCHAR(255) NOT NULL primary key COMMENT 'primary key',
-  createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
-  updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
-  name varchar(255) COMMENT 'User Name',
-  email varchar(255) COMMENT 'User Email',
-  picture varchar(255) COMMENT 'User Picture'
-) default charset utf8mb4 COMMENT '';
+-- Active: 1711167878661@@127.0.0.1@3306@adaptable_shaman_540684_db
+CREATE TABLE
+  IF NOT EXISTS accounts (
+    id VARCHAR(255) NOT NULL primary key COMMENT 'primary key',
+    createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+    updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+    name varchar(255) COMMENT 'User Name',
+    email varchar(255) COMMENT 'User Email',
+    picture varchar(255) COMMENT 'User Picture'
+  ) default charset utf8mb4 COMMENT '';
+
+CREATE TABLE
+  restaurants (
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+    updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+    name VARCHAR(255) NOT NULL,
+    imgUrl VARCHAR(1000) NOT NULL,
+    description VARCHAR(1000) NOT NULL,
+    visits INT NOT NULL DEFAULT 0,
+    isShutdown BOOLEAN NOT NULL DEFAULT false,
+    creatorId VARCHAR(255) NOT NULL,
+    FOREIGN KEY (creatorId) REFERENCES accounts (id) ON DELETE CASCADE
+  );
+
+DROP TABLE restaurants;
