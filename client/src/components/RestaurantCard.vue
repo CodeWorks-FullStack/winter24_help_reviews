6bf7d11663f3daa9327c0fee16161ac7401de2b3
Jeremy Fowler <jembyfowler@gmail.com>
2024-04-03 13:48:14 -0600

    abstracted to card, added authsettled

---
+++ client/src/components/RestaurantCard.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <img :src="restaurant.imgUrl" :alt="restaurant.name" class="img-fluid">
+    <div class="p-2">
+      <p class="text-success">
+        <b>{{ restaurant.name }}</b>
+      </p>
+      <p>{{ restaurant.description }}</p>
+      <div class="d-flex justify-content-between align-items-center">
+        <p>
+          <i class="mdi mdi-account-multiple me-1 text-success fs-3"></i>
+          <b>{{ restaurant.visits }}</b>
+          <span> recent visits</span>
+        </p>
+        <p>
+          <i class="mdi mdi-file-document-multiple me-1 text-success fs-3"></i>
+          <b>0</b>
+          <span> reports</span>
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+import { Restaurant } from '../models/Restaurant.js'
+export default {
+  props: {
+    restaurant: { type: Restaurant, required: true }
+  },
+  setup() {
+    return {}
+  }
+}
+</script>
+
+
+<style lang="scss" scoped></style>
--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -2,27 +2,7 @@
   <div class="container">
     <section class="row">
       <div v-for="restaurant in restaurants" :key="restaurant.id" class="col-md-4">
-        <div>
-          <img :src="restaurant.imgUrl" :alt="restaurant.name" class="img-fluid">
-          <div class="p-2">
-            <p class="text-success">
-              <b>{{ restaurant.name }}</b>
-            </p>
-            <p>{{ restaurant.description }}</p>
-            <div class="d-flex justify-content-between align-items-center">
-              <p>
-                <i class="mdi mdi-account-multiple me-1 text-success fs-3"></i>
-                <b>{{ restaurant.visits }}</b>
-                <span> recent visits</span>
-              </p>
-              <p>
-                <i class="mdi mdi-file-document-multiple me-1 text-success fs-3"></i>
-                <b>0</b>
-                <span> reports</span>
-              </p>
-            </div>
-          </div>
-        </div>
+        <RestaurantCard :restaurant="restaurant" />
       </div>
     </section>
   </div>
