5ebe98136420ea8bad8218432ee88004d6a8c682
Jeremy Fowler <jembyfowler@gmail.com>
2024-04-03 09:50:23 -0600

    set up restaurant model

---
+++ server/Models/Restaurant.cs
@@ -0,0 +1,11 @@
+namespace help_reviews.Models;
+
+public class Restaurant : RepoItem<int>
+{
+  public string Name { get; set; }
+  public string ImgUrl { get; set; }
+  public string Description { get; set; }
+  public int Visits { get; set; }
+  public bool IsShutdown { get; set; }
+  public string CreatorId { get; set; }
+}
