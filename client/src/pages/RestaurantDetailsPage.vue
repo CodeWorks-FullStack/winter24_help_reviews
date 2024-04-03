61a602688da83d5006c5ebc179c59f51afb89602
Jeremy Fowler <jembyfowler@gmail.com>
2024-04-03 14:30:05 -0600

    delete works!

--- client/src/pages/RestaurantDetailsPage.vue
+++ client/src/pages/RestaurantDetailsPage.vue
@@ -1,16 +1,56 @@
 <template>
-  <h1>RESTAURNAT PAGE</h1>
+  <div class="container">
+    <section v-if="restaurant" class="row my-3">
+      <div class="col-12 shadow-lg p-0">
+        <div class="d-flex justify-content-between">
+          <h1 class="text-success fs-3 m-1">{{ restaurant.name }}</h1>
+          <p v-if="restaurant.isShutdown" class="bg-danger text-light px-3 mb-0">
+            <i class="mdi mdi-close-circle"></i>
+            CURRENTLY SHUTDOWN
+          </p>
+        </div>
+        <img :src="restaurant.imgUrl" :alt="restaurant.name" class="restaurant-img">
+        <div class="p-2 mb-4">
+          <p>{{ restaurant.description }}</p>
+        </div>
+        <div class="p-2 d-flex justify-content-between">
+          <div class="d-flex">
+            <p>
+              <i class="mdi mdi-account-multiple me-1 text-success fs-3"></i>
+              <b>{{ restaurant.visits }}</b>
+              <span> recent visits</span>
+            </p>
+            <p>
+              <i class="mdi mdi-file-document-multiple me-1 text-success fs-3"></i>
+              <b>0</b>
+              <span> reports</span>
+            </p>
+          </div>
+
+          <div v-if="restaurant.creatorId == account.id">
+            <button class="btn btn-success"><i class="mdi mdi-door-open"></i> Re-Open</button>
+            <button @click="destroyRestaurant(restaurant.id)" class="btn btn-danger ms-3">
+              <i class="mdi mdi-delete-forever"></i>
+              Delete
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 
 <script>
 import Pop from '../utils/Pop.js'
 import { restaurantsService } from '../services/RestaurantsService.js'
-import { watch } from 'vue';
-import { useRoute } from 'vue-router';
+import { computed, watch } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+import { AppState } from '../AppState.js'
 export default {
   setup() {
     const route = useRoute()
+    const router = useRouter()
 
 
     async function getRestaurantById(restaurantId) {
@@ -26,10 +66,34 @@ export default {
       getRestaurantById(route.params.restaurantId)
     }, { immediate: true })
 
-    return {}
+    return {
+      restaurant: computed(() => AppState.activeRestaurant),
+      account: computed(() => AppState.account),
+
+      async destroyRestaurant(restaurantId) {
+        try {
+          const yes = await Pop.confirm()
+
+          if (!yes) return
+
+          await restaurantsService.destroyRestaurant(restaurantId)
+
+          router.push({ name: 'Home' })
+        }
+        catch (error) {
+          Pop.error(error);
+        }
+      }
+    }
   }
 }
 </script>
 
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.restaurant-img {
+  width: 100%;
+  height: 48vh;
+  object-fit: cover;
+}
+</style>
--- client/src/services/RestaurantsService.js
+++ client/src/services/RestaurantsService.js
@@ -4,6 +4,10 @@ import { logger } from "../utils/Logger.js"
 import { api } from "./AxiosService.js"
 
 class RestaurantsService {
+  async destroyRestaurant(restaurantId) {
+    const res = await api.delete(`api/restaurants/${restaurantId}`)
+    logger.log('DESTROYED RESTAURANT', res.data)
+  }
   async getRestaurantById(restaurantId) {
     const res = await api.get(`api/restaurants/${restaurantId}`)
     logger.log('GOT RESTAURANT', res.data)
