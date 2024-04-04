b2829f98c6d42b9197e22310530058334cf70919
Jeremy Fowler <jembyfowler@gmail.com>
2024-04-04 12:30:13 -0600

    created report

--- client/src/components/ModalComponent.vue
+++ client/src/components/ModalComponent.vue
@@ -7,25 +7,35 @@
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
-          <form>
+          <form @submit.prevent="createReport()">
+            <select v-model="editableReportData.restaurantId" class="form-select" aria-label="Select a Restaurant"
+              required>
+              <option v-for="restaurant in restaurants" :key="restaurant.id" :value="restaurant.id">
+                {{ restaurant.name }}
+              </option>
+            </select>
             <div class="mb-3">
               <label for="title" class="form-label">Report Title</label>
-              <input type="text" class="form-control" id="title" placeholder="Report Title..." maxlength="255" required>
+              <input v-model="editableReportData.title" type="text" class="form-control" id="title"
+                placeholder="Report Title..." maxlength="255" required>
             </div>
             <div class="mb-3">
               <label for="pictureOfDisgust" class="form-label">Report Picture Of Disgust</label>
-              <input type="url" class="form-control" id="pictureOfDisgust" placeholder="Report Picture Of Disgust..."
-                maxlength="255" required>
+              <input v-model="editableReportData.pictureOfDisgust" type="url" class="form-control" id="pictureOfDisgust"
+                placeholder="Report Picture Of Disgust..." maxlength="255">
             </div>
             <div class="mb-3">
               <label for="body" class="form-label">Report Body</label>
-              <textarea class="form-control" id="body" rows="3" maxlength="1000"></textarea>
+              <textarea v-model="editableReportData.body" class="form-control" id="body" rows="3"
+                maxlength="1000"></textarea>
+            </div>
+
+
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              <button type="submit" class="btn btn-primary">Submit Report</button>
             </div>
           </form>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary">Save changes</button>
         </div>
       </div>
     </div>
@@ -34,9 +44,26 @@
 
 
 <script>
+import { computed, ref } from 'vue'
+import { AppState } from '../AppState.js'
+import Pop from '../utils/Pop.js'
+import { reportsService } from '../services/ReportsService.js'
 export default {
   setup() {
-    return {}
+    const editableReportData = ref({})
+    return {
+      editableReportData,
+      restaurants: computed(() => AppState.restaurants),
+      async createReport() {
+        try {
+          await reportsService.createReport(editableReportData.value)
+          editableReportData.value = {}
+        }
+        catch (error) {
+          Pop.error(error);
+        }
+      }
+    }
   }
 }
 </script>
--- client/src/services/ReportsService.js
+++ client/src/services/ReportsService.js
@@ -4,6 +4,10 @@ import { logger } from "../utils/Logger.js"
 import { api } from "./AxiosService.js"
 
 class ReportsService {
+  async createReport(reportData) {
+    const res = await api.post('api/reports', reportData)
+    logger.log('CREATED REPORT', res.data)
+  }
   async getReportsByRestaurantId(restaurantId) {
     const res = await api.get(`api/restaurants/${restaurantId}/reports`)
     logger.log('GOT REPORTS', res.data)
