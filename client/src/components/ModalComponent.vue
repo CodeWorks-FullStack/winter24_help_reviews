ca8df3303c4b240871b2ad46a04395b4938f295b
Jeremy Fowler <jembyfowler@gmail.com>
2024-04-04 12:17:08 -0600

    getting restaurants when user logs in

--- client/src/components/ModalComponent.vue
+++ client/src/components/ModalComponent.vue
@@ -1,13 +1,27 @@
 <template>
   <div class="modal fade" id="reportModal" tabindex="-1" aria-labelledby="reportModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-      <div class="modal-content">
+    <div class="modal-dialog modal-xl">
+      <div class="modal-content ">
         <div class="modal-header">
-          <h1 class="modal-title fs-5" id="reportModalLabel">Modal title</h1>
+          <h1 class="modal-title fs-5 text-success" id="reportModalLabel">What do you say?</h1>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
-          ...
+          <form>
+            <div class="mb-3">
+              <label for="title" class="form-label">Report Title</label>
+              <input type="text" class="form-control" id="title" placeholder="Report Title..." maxlength="255" required>
+            </div>
+            <div class="mb-3">
+              <label for="pictureOfDisgust" class="form-label">Report Picture Of Disgust</label>
+              <input type="url" class="form-control" id="pictureOfDisgust" placeholder="Report Picture Of Disgust..."
+                maxlength="255" required>
+            </div>
+            <div class="mb-3">
+              <label for="body" class="form-label">Report Body</label>
+              <textarea class="form-control" id="body" rows="3" maxlength="1000"></textarea>
+            </div>
+          </form>
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
